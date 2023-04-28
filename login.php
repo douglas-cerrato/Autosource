3d3de4889cf8a70bc2e1d5404e365ce09df885a7
dougiefreshxd <douglascerrato2@gmail.com>
2023-04-28 14:38:14 -0700

    login.php update 4.28.23
    updated login php so that our buttons are actual buttons now with links to refer to. Added a home, about and contact link to our site for visiting as well

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>Autosource | Log in</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="style.css?v=4">
     </head>
 
     <body style="background-color:black">
@@ -9,13 +9,18 @@
             <span id="logo">
                 <img src="Autosource-Logo.png" alt="Autosource Logo">
             </span>
-            <span id="signup-button">Sign Up</span>
+            <a href="https://google.com">
+                <button id="signup-button">Sign Up</button>
+            </a>
         </div>
         <div id="Login-Bar">
         <br><br><br><br>Log in to Autosource Motors<br><br><br><br>
         <input type="text" id="text" placeholder="Email address or phone number"><br><br>
             <input type="password" id="text" placeholder="Password"><br><br>
-            <input type="submit" id="button" value="Log in">
+            <input type="submit" id="login_button" value="Log in"><br>
+            <a id="info_links" href="#">Home</a>
+            <a id="info_links" href="#">About</a>
+            <a id="info_links" href="#">Contact</a>
         </div>
     </body>
 </html>
