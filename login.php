927b7577319a69b9ca129a9b3c0f556e1789eded
dougiefreshxd <douglascerrato2@gmail.com>
2023-04-27 19:16:21 -0700

    update login.php
    Quick fix update

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
             <span id="signup-button">Sign Up</span>
         </div>
         <div id="Login-Bar">
-        <br><br><br><br>Log in to Mybook<br><br><br><br>
+        <br><br><br><br>Log in to Autosource Motors<br><br><br><br>
         <input type="text" id="text" placeholder="Email address or phone number"><br><br>
             <input type="password" id="text" placeholder="Password"><br><br>
             <input type="submit" id="button" value="Log in">
