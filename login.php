c6bbc4b8e6d125129833e54b5b29e81adfe392a2
dougiefreshxd <douglascerrato2@gmail.com>
2023-04-27 19:05:57 -0700

    Login.php Started
    Login page started in PHP, just making the layout of the site so far.

---
+++ login.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Autosource | Log in</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    <body style="background-color:black">
+        <div id="login-top-bar">
+            <span id="logo">
+                <img src="Autosource-Logo.png" alt="Autosource Logo">
+            </span>
+            <span id="signup-button">Sign Up</span>
+        </div>
+        <div id="Login-Bar">
+        <br><br><br><br>Log in to Mybook<br><br><br><br>
+        <input type="text" id="text" placeholder="Email address or phone number"><br><br>
+            <input type="password" id="text" placeholder="Password"><br><br>
+            <input type="submit" id="button" value="Log in">
+        </div>
+    </body>
+</html>
