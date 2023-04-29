5d7f7a56731d8744032cc103d992b0dc878ab4b7
dougiefreshxd <douglascerrato2@gmail.com>
2023-04-28 17:26:54 -0700

    signup.php made
    simple signup.php page made with login.php routing to the signup page. Next step is having the account creation input the data into the database and getting a homepage made with sessions as well

--- login.php
+++ login.php
@@ -1,26 +1,27 @@
-<html>
-    <head>
-        <title>Autosource | Log in</title>
-        <link rel="stylesheet" type="text/css" href="style.css?v=4">
-    </head>
+<!DOCTYPE html>    
+    <html>
+        <head>
+            <title>Autosource | Log in</title>
+            <link rel="stylesheet" type="text/css" href="style.css?v=4">
+        </head>
 
-    <body style="background-color:black">
-        <div id="login-top-bar">
-            <span id="logo">
-                <img src="Autosource-Logo.png" alt="Autosource Logo">
-            </span>
-            <a href="https://google.com">
-                <button id="signup-button">Sign Up</button>
-            </a>
-        </div>
-        <div id="Login-Bar">
-        <br><br><br><br>Log in to Autosource Motors<br><br><br><br>
-        <input type="text" id="text" placeholder="Email address or phone number"><br><br>
-            <input type="password" id="text" placeholder="Password"><br><br>
-            <input type="submit" id="login_button" value="Log in"><br>
-            <a id="info_links" href="#">Home</a>
-            <a id="info_links" href="#">About</a>
-            <a id="info_links" href="#">Contact</a>
-        </div>
-    </body>
-</html>
+        <body style="background-color:black">
+            <div id="login-top-bar">
+                <span id="logo">
+                    <img src="Autosource-Logo.png" alt="Autosource Logo">
+                </span>
+                <a href="signup.php">
+                    <button id="signup-button">Sign Up</button>
+                </a>
+            </div>
+            <div id="Login-Bar">
+                <br><br><br><br>Log in to Autosource Motors<br><br><br><br>
+                <input type="text" id="text" placeholder="Email address or phone number"><br><br>
+                    <input type="password" id="text" placeholder="Password"><br><br>
+                    <input type="submit" id="login_button" value="Log in"><br>
+                    <a id="info_links" href="#">Home</a>
+                    <a id="info_links" href="#">About</a>
+                    <a id="info_links" href="#">Contact</a>
+            </div>
+        </body>
+    </html>
---
+++ signup.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Autosource | Sign Up</title>
+            <link rel="stylesheet" type="text/css" href="style.css?v=3">
+        </head>
+
+        <body style="background-color: black">
+            <div id="signup-box">
+                <span id="logo">
+                    <img src="Autosource-Logo.png" alt="Autosource Logo" />
+                </span>
+                <br><br>Sign Up for Autosource<br><br><br><br>
+                <input type="text" id="signup_text" placeholder="First Name"><br><br>
+                <input type="text" id="signup_text" placeholder="Last Name"><br><br>
+                <input type="text" id="signup_text" placeholder="Email address"><br><br>
+                <input type="text" id="signup_text" placeholder="Phone Number"><br><br>
+                <input type="password" id="signup_text" placeholder="Password"><br><br>
+                <input type="password" id="signup_text" placeholder="Re-type Password"><br><br>
+
+                <input type="submit" id="createacc_button" value="Create Account"><br>
+        </body>
+    </html>
